819462effa1c55718ae8fb388eb20b11c4605712
Ashraf59 <ashrafams59b@gmail.com>
2023-07-08 18:03:44 +0600

    iPhone chnges by color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,34 @@
 'use-strict';
 
-/**
- * addEvent on element
- */
+// iPhone selecting function by color button
+const COLOR_BTNS = document.querySelectorAll('.color');
+COLOR_BTNS.forEach(dolor => {
+    dolor.addEventListener('click', () => {
+        let colorNameClass = dolor.className;
+        if(!dolor.classList.contains('active-color')){
+            let colorName = colorNameClass.slice(colorNameClass.indexOf('-') +1);
+            resetActiveBtns();
+            dolor.classList.add('active-color');
+            setNewColor(colorName)
+        }
+    });
+})
 
-// const addEventOnElem = function (elem, type, callback){
-//     if(elem.length > 1){
-//         for(let i = 0; i < elem.length; i++){
-            
-//             elem[i].addEventListener(type, callback)
-//         }
-//     }
-//     else{
-//         elem.addEventListener(type, callback)
-        
-//     }
-// }
+// resetting all color btns
+function resetActiveBtns(){
+    COLOR_BTNS.forEach(color => {
+        color.classList.remove('active-color');
+    });
+}
 
-
-/**
- * navbar toggle
- */
-
-// const navbar = document.querySelector("[data-navbar]");
-// const navbarLinks = document.querySelectorAll("[data-nav-link]");
-// const navbarToggle = document.querySelector("[data-nav-toggle]");
-
-
-// const toggleNav = function(){
-//     navbar.classList.toggle("active");
-//     navbarToggle.classList.toggle("active")
-// }
-
-// addEventOnElem(navbarToggle, "click", toggleNav)
-
-// const closeNav = function(){
-//     navbar.classList.remove("active");
-//     navbarToggle.classList.remove("active");
-// }
-
-// addEventOnElem(navbarLinks, "click", closeNav)
-
-
-/**
- * header active
- */
-
-
-// const header = document.querySelector("[data-header]");
-// const backTopBtn = document.querySelector("[data-back-top-btn]")
-
-// window.addEventListener("scroll", function(){
-//     if(window.scrollY >= 100){
-//         header.classList.add("active");
-//         backTopBtn.classList.add("active")
-//     }
-//     else{
-//         header.classList.remove("active");
-//         backTopBtn.classList.remove("active")
-//     }
-// })
+// set new color on the banner right 
+function setNewColor(color){
+    document.querySelector('.banner-right img').src = `assets/images/iphone-selection-by-color/iphone_${color}.png`;
+}
 
 
 
+// Refrigerator curve carousel
 const wrapper = document.querySelector('.wrapper')
 
 let pressed = false
