b591c2bd645a6d4a23c0467502fafb1c48bc3acd
Ashraf59 <ashrafams59b@gmail.com>
2023-09-04 18:32:55 +0600

    Camera and keyboard & Mouse category completed

---
+++ camera-category.js
@@ -0,0 +1,99 @@
+let products = [
+    {
+        name: 'Smart Watch',
+        image1: './assets/images/camera-category/nikon-camera-1.png',
+        image2: './assets/images/camera-category/nikon-camera-2.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'mi mini router',
+        image1: './assets/images/camera-category/canon-camera.png',
+        image2: './assets/images/camera-category/nikhon-camera.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Microphone',
+        image1: './assets/images/camera-category/Omega-camera.png',
+        image2: './assets/images/camera-category/sony-camera.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: '4k Camera',
+        image1: './assets/images/camera-category/canon-camera.png',
+        image2: './assets/images/camera-category/nikhon-camera.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Air Buds',
+        image1: './assets/images/camera-category/nikon-camera-1.png',
+        image2: './assets/images/camera-category/nikon-camera-2.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Smart Watch',
+        image1: './assets/images/camera-category/canon-camera.png',
+        image2: './assets/images/camera-category/nikhon-camera.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+]
+
+let product_list = document.querySelector('#products')
+
+renderProducts = (products) => {
+    products.forEach(e => {
+        let prod = `
+            <div class="col-4 col-md-6 col-sm-12">
+                <div class="product-card">
+                    <div class="product-card-img">
+                        <img src="${e.image1}" alt="">
+                        <img src="${e.image2}" alt="">
+                    </div>
+                    <div class="product-card-info">
+                        <div class="product-btn">
+                            <a href="./individualPage.html" target="_blank" class="btn-flat btn-hover btn-shop-now">shop now</a>
+                       
+                            <button class="btn-flat btn-hover btn-cart-add">
+                                <i class='bx bxs-cart-add'></i>
+                            </button>
+                            <button class="btn-flat btn-hover btn-cart-add">
+                                <i class='bx bxs-heart'></i>
+                            </button>
+                        </div>
+                        <div class="product-card-name">
+                        <span class="rating mt-5">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </span>
+                    </div>
+                        <div class="product-card-name">
+                            ${e.name}
+                        </div>
+                        <div class="product-card-price">
+                            <span><del>${e.old_price}</del></span>
+                            <span class="curr-price">${e.curr_price}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        product_list.insertAdjacentHTML("beforeend", prod)
+    })
+}
+
+renderProducts(products)
+renderProducts(products)
+
+let filter_col = document.querySelector('#filter-col')
+
+document.querySelector('#filter-toggle').addEventListener('click', () => filter_col.classList.toggle('active'))
+
+document.querySelector('#filter-close').addEventListener('click', () => filter_col.classList.toggle('active'))
---
+++ mouse-keyboard-category.js
@@ -0,0 +1,99 @@
+let products = [
+    {
+        name: 'Mouse',
+        image1: './assets/images/mouse-keyboard-category/mouse-1.png',
+        image2: './assets/images/mouse-keyboard-category/mouse-2.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Mouse',
+        image1: './assets/images/mouse-keyboard-category/mouse-3.png',
+        image2: './assets/images/mouse-keyboard-category/mouse-4.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Mouse',
+        image1: './assets/images/mouse-keyboard-category/mouse-4.png',
+        image2: './assets/images/mouse-keyboard-category/mouse-5.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Mouse',
+        image1: './assets/images/mouse-keyboard-category/mouse-6.png',
+        image2: './assets/images/mouse-keyboard-category/mouse-7.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Keyboard',
+        image1: './assets/images/mouse-keyboard-category/keyboard-2.png',
+        image2: './assets/images/mouse-keyboard-category/keyboard-3.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+    {
+        name: 'Keyboard',
+        image1: './assets/images/mouse-keyboard-category/keyboard-4.png',
+        image2: './assets/images/mouse-keyboard-category/keyboard-5.png',
+        old_price: '400',
+        curr_price: '300'
+    },
+]
+
+let product_list = document.querySelector('#products')
+
+renderProducts = (products) => {
+    products.forEach(e => {
+        let prod = `
+            <div class="col-4 col-md-6 col-sm-12">
+                <div class="product-card">
+                    <div class="product-card-img">
+                        <img src="${e.image1}" alt="">
+                        <img src="${e.image2}" alt="">
+                    </div>
+                    <div class="product-card-info">
+                        <div class="product-btn">
+                            <a href="./individualPage.html" target="_blank" class="btn-flat btn-hover btn-shop-now">shop now</a>
+                       
+                            <button class="btn-flat btn-hover btn-cart-add">
+                                <i class='bx bxs-cart-add'></i>
+                            </button>
+                            <button class="btn-flat btn-hover btn-cart-add">
+                                <i class='bx bxs-heart'></i>
+                            </button>
+                        </div>
+                        <div class="product-card-name">
+                        <span class="rating mt-5">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </span>
+                    </div>
+                        <div class="product-card-name">
+                            ${e.name}
+                        </div>
+                        <div class="product-card-price">
+                            <span><del>${e.old_price}</del></span>
+                            <span class="curr-price">${e.curr_price}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        product_list.insertAdjacentHTML("beforeend", prod)
+    })
+}
+
+renderProducts(products)
+renderProducts(products)
+
+let filter_col = document.querySelector('#filter-col')
+
+document.querySelector('#filter-toggle').addEventListener('click', () => filter_col.classList.toggle('active'))
+
+document.querySelector('#filter-close').addEventListener('click', () => filter_col.classList.toggle('active'))
